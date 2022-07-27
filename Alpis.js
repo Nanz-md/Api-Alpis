70e015c6a073b699c71bf03a82db6ea426ba0c75
Nanz-md <108403508+Nanz-md@users.noreply.github.com>
2022-07-27 20:02:14 +0700

    Update Alpis.js

--- Alpis.js
+++ Alpis.js
@@ -8,4 +8,4 @@
 //   Siapa Mahu Upload Jangan Lupa Credit :),
 //   Siapa Tidak Letak Credit Akan Ambil Tindakan
 //   
-//_______________________ ┏ Make By AlipBot ┓ _______________________\\
+//_______________________ ┏ Make By Nanz  ┓ _______________________\\
