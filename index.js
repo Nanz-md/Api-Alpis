7e2dceac6c45f4b45b0cd708cf402e22d6dd5652
Nanz-md <108403508+Nanz-md@users.noreply.github.com>
2022-07-27 20:01:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.listen(PORT, () => {
 	██╔══██╗██╔══╝  ╚════██║   ██║   ██╔══██║██╔═══╝ ██║
 	██║  ██║███████╗███████║   ██║   ██║  ██║██║     ██║
 	╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝
-			                              Make by Alip 
+			                              Make by Nanz 
 								 
  Server running on http://localhost:` + PORT)
 console.log(`Hello ${creator}`)
